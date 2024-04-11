29ba8e0f8de9cb80b412065097a88e53ab0cd13b
DiasoAndre <diaso.andre@outlook.com>
2024-04-11 13:26:18 -0300

    refactor: reorganiza as funções de leitura do .fs-config

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,13 @@ use std::process::exit;
 use std::io::Result;
 
 use module::read::*;
-use module::functions::Mode;
-
-use module::functions::set_mode;
+use module::functions::*;
 
 const WINDOWS_PATH: &str = "/mnt/c";
-const FS_CONFIG: &str = ".fs-config";
+pub const FS_CONFIG: &str = ".fs-config";
 
 fn main() {
-    println!("\n\nWindows File System to WSL!!\n\n");
+    println!("\n\nWindows Bridge to WSL\n\n");
     let args: Vec<String> = args().collect();
     //println!("{:?}", args);
     
@@ -21,41 +19,12 @@ fn main() {
         exit(1);
     }
 
-    let mode: Mode = set_mode(&args);
+    show_config(&args);
 
-    match mode {
-        Mode::Default => println!("Default mode"),
-        Mode::Windows => {
-            let path = args[2].clone();
-
-            if verify_path(&path) && is_file(&path) {
-                println!("{path} Is File!!");
-            } else {
-                println!("{path} Is Folder :(");
-            }
-        },
-        Mode::None => println!("Please, select a mode\nUse: fs [mode] [path]")
-    }
-
-    let fs_config: Result<Vec<(String, String)>> = return_fs_config();
-    match fs_config {
-        Ok(lines) => {
-            for value in lines.iter() {
-                if value.0 == "windows_user_path" {
-                    let mut windows_user_path = String::from(WINDOWS_PATH);
-                    windows_user_path.push_str(&value.1);
-                    println!("{windows_user_path}");
-                }
-            }
-        },
-        Err(e) => {
-            println!("Erro no processo de leitura de arquivo de configuração\n{e}");
-            exit(1);
-        },
-    }
+    
 }
 // resolve para um array de tuplas string com (chave, valor)
-fn return_fs_config() -> Result<Vec<(String, String)>> {
+fn get_file_config() -> Result<Vec<(String, String)>> {
     match read_file(FS_CONFIG) {
         Ok(out) => {
             let lines: Vec<&str> = out.split("\n").collect();
@@ -84,4 +53,23 @@ fn return_fs_config() -> Result<Vec<(String, String)>> {
         Err(e) => Err(e),
     }
     
+}
+
+fn show_config(args: &Vec<String>) {
+    let fs_config: Result<Vec<(String, String)>> = get_file_config();
+    match fs_config {
+        Ok(lines) => {
+            for value in lines.iter() {
+                if value.0 == "windows_user_path" {
+                    let mut windows_user_path = String::from(WINDOWS_PATH);
+                    windows_user_path.push_str(&value.1);
+                    println!("Windows User Path = {windows_user_path}");
+                }
+            }
+        },
+        Err(e) => {
+            println!("Erro no processo de leitura de arquivo de configuração\n{e}");
+            exit(1);
+        },
+    }
 }
