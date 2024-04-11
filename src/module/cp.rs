847e2dbb743ba94f0938c1f4630db749377a9946
DiasoAndre <diaso.andre@outlook.com>
2024-04-11 16:55:19 -0300

    feat: adiciona função para copiar, somente arquivos.

--- src/module/cp.rs
+++ src/module/cp.rs
@@ -1,9 +1,9 @@
 use std::fs;
-use std::path::Path;
 use std::process::exit;
+use fs_extra::dir::{copy as fs_extra_copy, CopyOptions};
 
-mod config;
-use config::*;
+use super::config::*;
+use super::read::*;
 
 /*
     pub enum Command {
@@ -19,6 +19,25 @@ use config::*;
     }
 */
 
-pub fn copy(config: Config) {
-    fs::copy(config.path, config.command);
+pub fn copy(config: &Config) {
+    if verify_path(&config.win_path) {
+        match &config.command {
+            Command::Copy(value) => {
+                if is_file(&config.win_path) {
+                    fs::copy(&config.win_path, value).expect("Falha ao copiar arquivo");
+                } else {
+                    println!("\nNão é um arquivo, tentando copiar...\n");
+                    let options = CopyOptions::new();
+                    fs_extra_copy(&config.win_path, value, &options).expect("Falha ao copiar diretório");
+                }
+            },
+            _ => {
+                println!("O comando não é uma cópia.");
+                exit(1);
+            }
+        }
+    } else {
+        println!("{} não existe. Verifique se o arquivo foi movido ou renomeado", config.win_path);
+        exit(1);
+    }
 }
